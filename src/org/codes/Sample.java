359942608f6593a9665879ae69a623ce61526e16
RD9876 <renudev2706@gmail.com>
2021-03-24 12:47:50 +0530

    added to B

--- src/org/codes/Sample.java
+++ src/org/codes/Sample.java
@@ -5,6 +5,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Javas");
 		System.out.println("New line");
+		String name = "Greens";
+		System.out.println(name);
 	}
 
 }
